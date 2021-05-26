def5ff288593fbed60233f8cd3dc290af29a04d7
Jaleab <javier.abarca@ucr.ac.cr>
2021-05-25 22:45:02 -0600

    Condicionales para despliegue segun estado del usuario y falta libreria mariadb/mysql.h para connector.cpp

--- codigo/usuarioVerificado.cpp
+++ codigo/usuarioVerificado.cpp
@@ -14,15 +14,28 @@ int main(int argc, char* argv[], char** envp) {
         cout << "<P><EM>Unable to open data file, sorry!</EM>\n";
     }
     else {
-        cout << "Set-Cookie:estado = registrado;\r\n";
+        string correo = getenv("QUERY_STRING");
+        //cout << correo << "<br>";
+        if(correo.find("ucr.ac.cr") != string::npos){
+            cout << "Set-Cookie:estado = registrado;\r\n";
+        }
+        else{
+            cout << "Set-Cookie:estado = noRegistrado;\r\n";
+        }
         cout << "Content-Type: text/html\n\n";
         string line = "";
         while(getline(htmlFile, line)){
             cout << line +"\n";
         }
-    cout << "\n\n\n\n\n";
-    htmlFile.close();
-    cout << getenv("HTTP_COOKIE") << "<br>";
+        htmlFile.close();
+        cout << getenv("HTTP_COOKIE") << "<br>";
+        string hilera = getenv("HTTP_COOKIE");
+        if(hilera.find("estado=registrado") != string::npos){
+            cout << "Usuario esta registrado <br>";
+        }
+        else{
+            cout << "El usuario no esta registrado <br>";
+        }
     }
     return 0;
 }
