5fc5154cc003d4b73afe91b52bbec23e894d36ce
DESKTOP-E9C33I4\Yerlin <ledemadrigal@gmail.com>
2021-05-30 16:37:12 -0600

    prueba del formato del cpp

--- codigo/formularioArticulo.cpp
+++ codigo/formularioArticulo.cpp
@@ -3,11 +3,52 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "unificadorVistasHTML.h"
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    unificadorVistasHTML unificadorVista;
-    unificadorVista.mostrarVistaCompleta("articulo.html");
+    ifstream htmlFile;
+    string line = "";
+    // Insertar header en el body
+    htmlFile.open("../html/headerInsert.html");
+    if(!htmlFile.is_open()) {
+        cout << "Content-Type:text/html\n";
+        cout << "<TITLE>Failure</TITLE>\n";
+        cout << "<P><EM>Unable to open data file, sorry!</EM>\n";
+    }
+    else {
+        cout << "Content-Type: text/html\n\n";
+        while(getline(htmlFile, line)){
+            cout << line +"\n";
+        }
+        htmlFile.close();
+        
+        // Insertar contenido en el body
+        htmlFile.open("../html/formularioArticulo.html");
+        if(!htmlFile.is_open()) {
+            cout << "<TITLE>Failure</TITLE>\n";
+            cout << "<P><EM>Unable to open data file, sorry!</EM>\n";
+        }
+        else {
+            line = "";
+            while(getline(htmlFile, line)){
+                cout << line +"\n";
+            }
+            htmlFile.close();
+
+            // Insertar footer en el body
+            htmlFile.open("../html/footerInsert.html");
+            if(!htmlFile.is_open()) {
+                cout << "<TITLE>Failure</TITLE>\n";
+                cout << "<P><EM>Unable to open data file, sorry!</EM>\n";
+            }
+            else {
+                line = "";
+                while(getline(htmlFile, line)){
+                    cout << line +"\n";
+                }
+                htmlFile.close();
+            }
+        }
+    }
     return 0;
 }
