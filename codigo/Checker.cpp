967c63e3b422d3fcac7401c072a98839b9f54991
Jaleab <javier.abarca@ucr.ac.cr>
2021-06-03 00:59:39 -0600

    Arreglo en hileras y caracteres buscados en checker

--- codigo/Checker.cpp
+++ codigo/Checker.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
    void Checker::checkParameter(string& parameter){
-      string key[14] = {"and","*","="," ","%0a","%","/","union","|","&","^" ,"#","/*","*/" };
-      for (int i = 0; i < 14; ++i){
+      // Le quite and, union y el espacio en blanco " ".
+      string key[11] = {"*","=","%0a","%","/","|","&","^" ,"#","/*","*/" };
+      for (int i = 0; i < 11; ++i){
          if(parameter.find(key[i]) != string::npos){
 		for(char letter : key[i]){
 			parameter.erase(remove(parameter.begin(), parameter.end(), letter), parameter.end());
 		}
-		//cout << parameter << "<br> <br>";
          }
       }
    }
