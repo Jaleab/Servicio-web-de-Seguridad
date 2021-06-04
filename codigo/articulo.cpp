8c33063155b65fdb0f33e845903700d48ceea77a
Polina <polina.tarassenko@ucr.ac.cr>
2021-06-03 19:34:22 -0600

    Se agreag la funcionalidad de detalle del articulo

--- codigo/articulo.cpp
+++ codigo/articulo.cpp
@@ -1,11 +1,35 @@
+// g++ `mysql_config --cflags --libs` articulo.cpp ConectorModular.cpp -o ../cgi-bin/articulo.cgi
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include "ConectorModular.h"
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[], char** envp) {
+
+    string queryString; 
+    string infoArticulo;
+
+    queryString = getenv("QUERY_STRING");
+    infoArticulo = queryString.substr(queryString.find("id=")+3);
+    //int idArticulo = stoi(infoArticulo);
+    
+
+    ConectorModular* conectorModularPtr;
+    MYSQL* con;
+    MYSQL_RES *res; // the results
+    MYSQL_ROW row;  // the results rows (array)
+
+    con = conectorModularPtr->connection();
+    string query = "SELECT * FROM Articulo WHERE articuloId ='"+infoArticulo+"';";
+    res = conectorModularPtr->query(con, query.c_str());
+
+    row = mysql_fetch_row(res);
+
+
     ifstream htmlFile;
     string line = "";
     // Insertar header en el body
@@ -21,19 +45,70 @@ int main(int argc, char* argv[]) {
             cout << line +"\n";
         }
         htmlFile.close();
-        
-        // Insertar contenido en el body
-        htmlFile.open("../html/articulo.html");
-        if(!htmlFile.is_open()) {
-            cout << "<TITLE>Failure</TITLE>\n";
-            cout << "<P><EM>Unable to open data file, sorry!</EM>\n";
-        }
-        else {
-            line = "";
-            while(getline(htmlFile, line)){
-                cout << line +"\n";
-            }
-            htmlFile.close();
+
+            // Insertar contenido en el body
+        cout << "<div class='card card3' style='width: 50rem; margin-top: 30px; margin-left:20%'>";
+        cout << "<div class='card-body'>";
+        cout << "<div>";
+        cout << "<div class='row'>";
+        cout << "<div class='col'>";
+        cout << "<div class='form-group'>";
+        cout << "<label class='col-form-label' for='nombreArticuloInput'>Nombre del artículo</label>";
+        cout << "<input type='text' title='Nombre del articulo' class='form-control' placeholder='";
+        cout << row[1];
+        cout << "'id=nombreArticuloInput disabled>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "<div>";
+        cout << "<div class='row'>";
+        cout << "<div class='col'>";
+        cout << "<div class='form-group'>";
+        cout << "<label class='col-form-label' for='autorArticuloInput'>Autor del artículo</label>";
+        cout << "<input type='text' title='Autor del articulo' class='form-control' placeholder='";
+        cout << row[4];
+        cout << "'id=autorArticuloInput disabled>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "<div>";
+        cout << "<div class='row'>";
+        cout << "<div class='col'>";
+        cout << "<div class='form-group'>";
+        cout << "<label class='col-form-label' for='descripcionInput'>Descripción del artículo</label>";
+        cout << "<textarea title='Descripción del articulo' class='form-control' placeholder='";
+        cout << row[2];
+        cout << "' id='descripcionInput' disabled></textarea>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "<div>";
+        cout << "<div class='row'>";
+        cout << "<div class='col'>";
+        cout << "<div class='form-group'>";
+        cout << "<label class='col-form-label' for='precioInput'>Precio del artículo</label>";
+        cout << "<input type='text' title='Precio del artículo' class='form-control' placeholder='";
+        cout << row[3];
+        cout << "' id='precioArticuloInput' disabled>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "<div style='text-align:center;'>";
+        cout << "<a type='button' class='btn btn-primary' style='width: 200px;display:inline-block' href='http://172.24.131.136/cgi-bin/resultadoBusqueda.cgi'>Regresar</a>";
+        cout << "<button type='button' class='btn btn-primary' style='width: 200px'>Agregar</button>";
+        cout << "</div>";
+        cout << "</div>";
+        cout << "</div>";
+
+            // clean up the database result
+            mysql_free_result(res);
+            
+            // close database connection
+            mysql_close(con);
 
             // Insertar footer en el body
             htmlFile.open("../html/footerInsert.html");
@@ -48,7 +123,7 @@ int main(int argc, char* argv[]) {
                 }
                 htmlFile.close();
             }
-        }
+        //}
     }
     return 0;
 }
